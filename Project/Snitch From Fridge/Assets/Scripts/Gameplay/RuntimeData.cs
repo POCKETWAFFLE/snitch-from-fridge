7de3c4fa156842784294917bd2e68207ea51c186
k.yakupova <yakupova.k.v@yandex.by>
2018-01-21 17:41:03 +0300

    + add quit and game time

---
+++ Project/Snitch From Fridge/Assets/Scripts/Gameplay/RuntimeData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeData : MonoBehaviour {
+
+    public static float GAME_TIME;
+
+	// Use this for initialization
+	void Start () {
+        GAME_TIME = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GAME_TIME += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
